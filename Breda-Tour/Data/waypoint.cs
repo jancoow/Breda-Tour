20f70c54794e6f34d9a9c150f9b4b83d62ba8e38
jancoow <jancokock@gmail.com>
2016-01-01 16:34:31 +0100

    Changed lan-lon to geopoint. Removed debug lines

--- Breda-Tour/Data/waypoint.cs
+++ Breda-Tour/Data/waypoint.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
+using Windows.Devices.Geolocation;
 
 namespace Breda_Tour.Data
 {
     class Waypoint
     {
-        public float Lat { get; private set; }
-        public float Long { get; private set; }
+        public Geopoint Position { get; private set; }
         public String Title { get; private set; }
         public String Description { get; private set; }
         public List<Image> Photos { get; private set; }
 
         public Waypoint(float Lat, float Long, String Title, String Description, List<string> pictures)
         {
-            this.Lat = Lat;
-            this.Long = Long;
+            this.Position = new Geopoint(new BasicGeoposition() { Altitude = 0, Latitude = Lat, Longitude = Long });
             this.Title = Title;
             this.Description = Description;
             this.Photos = new List<Image>();
@@ -32,10 +31,6 @@ namespace Breda_Tour.Data
                             Source = new BitmapImage(new Uri("file://Storages/images/" + source)),
                             Name = source
                         });
-                    }
-                    foreach (Image i in Photos)
-                    {
-                        System.Diagnostics.Debug.WriteLine(i.Name);
                     }
                 });
             }
