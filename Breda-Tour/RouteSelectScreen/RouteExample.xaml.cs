d109ce277228c19b2a4d1ebc5ac7514d9ba016e0
guusvdongen <guus.van.dongen@hotmail.com>
2016-01-03 16:09:11 +0100

    RouteSelect aangepast

---
+++ Breda-Tour/RouteSelectScreen/RouteExample.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Breda_Tour.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Breda_Tour.RouteSelectScreen
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RouteExample : Page
+    {
+        Route route;
+        public string Waypoints { get; set; } 
+
+        public RouteExample()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            route = e.Parameter as Route;
+            Waypoints = "Aantal waypoints: " + 0;
+            this.DataContext = route;
+            //if (route.Waypoints.Count != 0){
+              //  waypoints = route.Waypoints.Count;
+            //}
+        }
+    }
+}
