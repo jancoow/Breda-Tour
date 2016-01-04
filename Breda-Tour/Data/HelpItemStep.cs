a377c8e24f10bfe342fa1729a4e8ce6925d4f1ac
jancoow <jancokock@gmail.com>
2016-01-04 12:06:18 +0100

    Added data classes for help items

---
+++ Breda-Tour/Data/HelpItemStep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breda_Tour.Data
+{
+    class HelpItemStep
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string ImageSource { get; private set; }
+
+        public HelpItemStep(string Title, string Description, string Imagesource)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.ImageSource = ImageSource;
+        }
+
+    }
+}
---
+++ Breda-Tour/Data/HelpPageDatabase.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breda_Tour.Data
+{ 
+
+    class HelpPageDatabase
+    {
+        private List<HelpItem> HelpItems;
+        public HelpPageDatabase()
+        {
+            HelpItems = new List<HelpItem>();
+            readHelpItems();
+
+        }
+        private void readHelpItems()
+        {
+            Task.Run(() =>
+            {
+                string json = File.ReadAllText("Storage/helpitems/helpitems.json");
+                JObject JsonObject = JObject.Parse(json);
+                IList<JToken> JsonList = JsonObject["HelpItems"].ToList();
+                foreach (JToken helpitem in JsonList)
+                {
+                    HelpItems.Add(JsonConvert.DeserializeObject<HelpItem>(helpitem.ToString()));
+                }
+                System.Diagnostics.Debug.WriteLine(HelpItems[0].HelpItemSteps[0].Title);
+
+            });
+        }
+    }
+}
